ee0be06bf1a6a577999a749e15393490591cacde
vastrak <mailboxk@aol.com>
2019-06-26 09:47:04 +0200

    inner join queries - step 4

--- hibernate006/src/test/java/com/vastrak/hibernate006/model/ModelTest.java
+++ hibernate006/src/test/java/com/vastrak/hibernate006/model/ModelTest.java
@@ -5,7 +5,10 @@ import static org.assertj.core.api.Assertions.catchThrowable;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Formatter;
 import java.util.List;
 
 import javax.persistence.Query;
@@ -107,10 +110,9 @@ public class ModelTest {
 		session = HibernateUtil.getSessionFactory().openSession();
 		Query query = session.getNamedQuery("technician.findAll");
 		List<Technician> technicians = query.getResultList();
-		
-		assertThat(technicians).isNotNull();
+
 		assertThat(technicians.size()).isEqualTo(3);
-		
+
 		logger.info(">>>>> Technicians list size: " + technicians.size()); // must be 3
 		session.close();
 	}
@@ -125,7 +127,6 @@ public class ModelTest {
 		query.setParameter("name", name);
 		List<Technician> technicians = query.getResultList();
 
-		assertThat(technicians).isNotNull();
 		assertThat(technicians.get(0).getName()).isEqualTo(name);
 
 		logger.info(">>>>> FindByName: " + name + "=" + technicians.get(0).getName());
@@ -144,12 +145,118 @@ public class ModelTest {
 		query.setParameter("name", "%" + name + "%");
 		List<Technician> technicians = query.getResultList();
 
-		assertThat(technicians).isNotNull();
 		assertThat(technicians.get(0).getName()).containsOnlyOnce(name);
 
 		logger.info(">>>>> FindLikeName: " + name + "=" + technicians.get(0).getName());
 
 		session.close();
+	}
+
+	@Test
+	public void test006_findByDeployBetweenDates() throws ParseException {
+
+		// prev. Parse throws ParseException
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date frmDate = format.parse("2019-03-29 00:00:00.0");
+		Date endDate = format.parse("2019-05-01 00:00:00.0");
+		//
+		session = HibernateUtil.getSessionFactory().openSession();
+		String hql = "from Device de where de.deploy between :frmDate and :endDate";
+		org.hibernate.query.Query<Device> query = session.createQuery(hql, Device.class);
+		query.setParameter("frmDate", frmDate);
+		query.setParameter("endDate", endDate);
+		// Comment require enable property name "use_sql_comments" set to true
+		// in hibernate.cfg.xml
+		query.setComment(">> HQL filter between dates ");
+		List<Device> results = query.list(); // never return null
+
+		assertThat(results.size()).isEqualTo(2);
+
+		for (Device de : results) {
+			logger.info(">> " + de.getName() + " " + de.getDeploy());
+		}
+
+		session.close();
+	}
+
+	@Test
+	@SuppressWarnings("unchecked")
+	public void test007_projectionOnDevice() {
+
+		// projection query with select clause
+		String hql = "select upper(de.name), de.deploy from Device de";
+
+		session = HibernateUtil.getSessionFactory().openSession();
+		Query query = session.createQuery(hql);
+		List<Object[]> results = query.getResultList();
+
+		assertThat(results.size()).isEqualTo(4); //
+
+		// for retrieved this!
+		for (Object[] o : results) {
+			String name = (String) o[0];
+			Date deploy = (Date) o[1];
+			logger.info(String.format(">> name: %s, deploy %s", name, deploy));
+		}
+
+		session.close();
+	}
+
+	@Test
+	public void test008_obtainingUniqueResultDevice() throws ParseException {
+
+		// prev. Parse throws ParseException
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date frmDate = format.parse("2019-04-01 00:00:00.0");
+
+		String hql = "from Device de where de.deploy>:frmDate";
+
+		session = HibernateUtil.getSessionFactory().openSession();
+		org.hibernate.query.Query<Device> query = session.createQuery(hql, Device.class);
+		query.setParameter("frmDate", frmDate);
+		query.setMaxResults(1); // or method will throw a NonUniqueResultException
+		Device device = query.uniqueResult(); // can return null, we need check this option
+
+		assertThat(device).isNotNull();
+
+		logger.info(String.format(">> name: %s, deploy %s", device.getName(), device.getDeploy()));
+
+		session.close();
+
+		// not necessarily Hibernate just to pick off the first result and return it
+		// either set the maximum results of the HQL query to 1,
+		// or obtain the first object from the result list.
+
+	}
+
+	@Test
+	@SuppressWarnings("unchecked")
+	public void test009_innerJoinTechnicianIssue() {
+
+		// return resultset: (Technician.name + Issue.created + Issue.closed) x all
+		// issues
+		String hql = "select te.name, iss.created, iss.closed from Technician te inner join te.issues as iss";
+
+		session = HibernateUtil.getSessionFactory().openSession();
+		Query query = session.createQuery(hql);
+		List<Object[]> results = query.getResultList();
+
+		logger.info(">>> results= " + results.size());
+
+		assertThat(results.size()).isEqualTo(9); // igual a la cantidad de registros de issue
+
+		StringBuilder sb = new StringBuilder();
+		for (Object[] o : results) {
+			String name = (String) o[0];
+			Date created = (Date) o[1];
+			Date closed = (Date) o[2];
+			sb.append(String.format(">> name: %-20s, attended issue " + "created %s and closed %s\n", name, created,
+					closed));
+		}
+		logger.info(sb.toString());
+
+		session.close();
+
 	}
 	
 }
